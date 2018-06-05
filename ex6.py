03ebdd9ed8c1eb0356d5f50c12c67676316e3613
Ma-Min-Min <minmin12697@gmail.com>
2018-06-05 22:16:37 +0630

    Changed

--- ex6.py
+++ ex6.py
@@ -2,7 +2,7 @@ types_of_people =10
 x= f"There are {types_of_people} types of people."
 binary ="binary"
 do_not="don't"
-y= f"Those who know {binary} and those who {do _not}."
+y= f"Those who know {binary} and those who {do_not}."
 print(x)
 print(y)
 
