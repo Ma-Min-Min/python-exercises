3c6b32c0df0705ef0bf1f7e0c8dc39d981d27248
Ma-Min-Min <minmin12697@gmail.com>
2018-06-11 00:03:33 +0630

    Changed23

--- ex23.py
+++ ex23.py
@@ -13,8 +13,8 @@ def print_line(line, encoding, errors) :
     raw_bytes = next_lang.encode (encoding, errors=errors)
     cooked_string = raw_bytes.decode (encoding, errors=errors)
 
-            print(raw_bytes, "<===>", cooked_string)
+            print (raw_bytes, "<===>", cooked_string)
 
-        language = open ("languages.txt", encoding="utf-8")
+        languages = open ("languages.txt", encoding="utf-8")
 
         main (languages, encoding, error)
